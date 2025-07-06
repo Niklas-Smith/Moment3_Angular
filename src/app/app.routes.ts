2795b5034a10b5e690a9bd2bf81b266281a4c21c
Niklas Smith <niklas_smith@hotmail.com>
2025-07-07 00:10:15 +0200

    added routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConvertComponent } from './convert/convert.component';
+import { NonExistentComponent } from './non-existent/non-existent.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: "home" , component: HomeComponent},
+{path: "about" , component: AboutComponent},
+{path: "convert" , component:ConvertComponent},
+{path: "", redirectTo: "/home", pathMatch: "full"},
+{path: "404", component: NonExistentComponent},
+{path: "**", component:NonExistentComponent}
+
+];
---
+++ src/app/non-existent/non-existent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-non-existent',
+  imports: [],
+  templateUrl: './non-existent.component.html',
+  styleUrl: './non-existent.component.css'
+})
+export class NonExistentComponent {
+
+}
