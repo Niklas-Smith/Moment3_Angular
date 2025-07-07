3419d1f0e3bc9918aaff815f3d81889940cb64dc
Niklas Smith <niklas_smith@hotmail.com>
2025-07-07 07:06:21 +0200

    Made convert page (feet and meters and Celsius and fahrenheit)

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+convert: number = 0;
+result: number = 0;
+feet: number = 0;
+result2: number = 0;
+
+convertFtoC(): void{
+
+  this.result = (this.convert - 32) * 5/9
+}
+
+convertCtoF(): void{
+
+  this.result = (this.convert * 9/5) + 32
+}
+
+feetToMeter(): void{
+
+this.result2 = (this.feet / 3.28)
 
 }
+
+meterToFeet(): void{
+
+this.result2 = (this.feet * 3.28)
+
+}
+
+
+}
+
